f95ff2a9f629b048319f32bd5dc4164a74bc97ba
Gergely Vaczi <48059107+vaczigergely@users.noreply.github.com>
2021-08-13 12:52:32 +0200

    Working example

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const APP_PORT = 5555
+
+const server = app.listen(APP_PORT, () => {
+  console.log(`App running on port ${APP_PORT}`)
+})
+
+const io = require('socket.io')(server)
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('chatter', (message) => {
+    console.log('chatter : ', message)
+    io.emit('chatter', message)
+  })
+})
